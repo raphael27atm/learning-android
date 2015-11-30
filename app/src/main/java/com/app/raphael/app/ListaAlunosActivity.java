155a73f81df2eca9d009180b2fb11defd9927b6c
Marlon Raphael <raphael27atm@gmail.com>
2015-11-30 14:23:17 -0300

    add event click

--- app/src/main/java/com/app/raphael/app/ListaAlunosActivity.java
+++ app/src/main/java/com/app/raphael/app/ListaAlunosActivity.java
@@ -2,8 +2,11 @@ package com.app.raphael.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * Created by raphael on 30/11/15.
@@ -21,5 +24,20 @@ public class ListaAlunosActivity extends Activity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, alunos);
 
         lista.setAdapter(adapter);
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
+                Toast.makeText(ListaAlunosActivity.this, "A posição é " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        lista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapter, View view, int position, long id) {
+                Toast.makeText(ListaAlunosActivity.this, "Aluno clicado é " + adapter.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
     }
 }
